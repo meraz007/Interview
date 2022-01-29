0e96ce826e167002ee7a799a490dd43965bdc5c6
saiful Islam <saifulmeraz897@gmail.com>
2022-01-29 16:09:03 +0600

    adding aleart

--- chart-of-accounts/src/views/Home.vue
+++ chart-of-accounts/src/views/Home.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="home">
-    <h1>Chart of accounts</h1>
+    <div class="container">
+      <h1>Chart of accounts</h1>
+    <p>
+      <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Assets
+      </button>
+      <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Liabilities & Credit Cards
+      </button>
+      <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Income
+      </button>
+      <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Expenses
+      </button>
+      <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Equity
+      </button>
+      
+    </p>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+          Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
